4938cfa750c2d148d18c28915d9dacb086bbe9fe
ivanberry <tabvim.dev@gmail.com>
2018-06-11 20:44:36 +0800

    Add things should do after request

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import Routes from './routes';
@@ -15,18 +16,38 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('token');
   const refreshToken = localStorage.getItem('refreshToken');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       'x-token': token,
       'x-refresh-token': refreshToken,
-      // authorization: token ? `Bearer ${token}` : '',
     },
   };
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    const context = operation.getContext();
+    const {
+      response: { headers },
+    } = context;
+
+    if (headers) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refresh-token');
+
+      if (token) {
+        localStorage.setItem('x-token', token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem('x-refresh-token', refreshToken);
+      }
+    }
+    return response;
+  });
 });
 
 const client = new ApolloClient({
