20741f1a6a296dae63088c1437e056ed2fa6aaae
ivanberry <tabvim.dev@gmail.com>
2018-06-11 12:15:07 +0800

    Upgrade Apollo client to version 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:8080/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 const App = () => (
