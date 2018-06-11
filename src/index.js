7dd70e110026f2d50605cf30dbdba7f8c92b9daf
ivanberry <tabvim.dev@gmail.com>
2018-06-11 16:49:56 +0800

    Add custom request headers

--- src/index.js
+++ src/index.js
@@ -17,11 +17,14 @@ const httpLink = createHttpLink({
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      'x-token': token,
+      'x-refresh-token': refreshToken,
+      // authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
