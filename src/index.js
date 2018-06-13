81a98d08231f6461a17362619f158933407b380a
ivanberry <tabvim.dev@gmail.com>
2018-06-13 10:40:05 +0800

    Add user authenticated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, concat } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import Routes from './routes';
@@ -27,12 +27,14 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const afterwareLink = new ApolloLink((operation, forward) => {
-  return forward(operation).map((response) => {
+const setLocalTokens = new ApolloLink((operation, forward) =>
+  forward(operation).map((response) => {
     const context = operation.getContext();
     const {
       response: { headers },
     } = context;
+
+    console.log('set local tokens');
 
     if (headers) {
       const token = headers.get('x-token');
@@ -47,11 +49,11 @@ const afterwareLink = new ApolloLink((operation, forward) => {
       }
     }
     return response;
-  });
-});
+  }),
+);
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: concat(authLink, httpLink, setLocalTokens),
   cache: new InMemoryCache(),
 });
 
