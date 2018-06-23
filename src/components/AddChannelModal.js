4630f3c63b2e22172213a5ad6bb7ad460a4464f1
ivanberry <tabvim.dev@gmail.com>
2018-06-23 10:28:58 +0800

    .feat Add channel modal

--- src/components/AddChannelModal.js
+++ src/components/AddChannelModal.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { withFormik } from 'formik';
+import { findIndex } from 'lodash';
 import { Modal, Input, Button, Form } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { compose, graphql } from 'react-apollo';
+import { allTeamsQuery } from '../graphql/team';
 
 const AddChannelModal = ({
   open,
@@ -13,33 +15,34 @@ const AddChannelModal = ({
   handleBlur,
   handleSubmit,
   isSubmitting,
+  teamId
 }) => (
-  <Modal open={open} onClose={onClose}>
-    <Modal.Header>Create a new channel?</Modal.Header>
-    <Modal.Content>
-      <Form>
-        <Form.Field>
-          <Input
-            value={values.name}
-            name="name"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder="Channel name..."
-            fluid
-          />
-        </Form.Field>
-        <Form.Group widths="equal">
-          <Button disabled={isSubmitting} onClick={onClose} fluid>
-            Cancel
+    <Modal open={open} onClose={onClose}>
+      <Modal.Header>Create a new channel?</Modal.Header>
+      <Modal.Content>
+        <Form>
+          <Form.Field>
+            <Input
+              value={values.name}
+              name="name"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Channel name..."
+              fluid
+            />
+          </Form.Field>
+          <Form.Group widths="equal">
+            <Button disabled={isSubmitting} onClick={onClose} fluid>
+              Cancel
           </Button>
-          <Button disabled={isSubmitting} onClick={handleSubmit} fluid>
-            Create
+            <Button disabled={isSubmitting} onClick={handleSubmit} fluid>
+              Create
           </Button>
-        </Form.Group>
-      </Form>
-    </Modal.Content>
-  </Modal>
-);
+          </Form.Group>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  );
 
 AddChannelModal.propTypes = {
   open: PropTypes.bool.isRequired,
@@ -53,24 +56,42 @@ AddChannelModal.propTypes = {
   isSubmitting: PropTypes.bool.isRequired,
 };
 
-const createTeamMutation = gql`
+const createChannelMutation = gql`
   mutation($teamId: Int!, $name: String!) {
-    createChannel(teamId: $teamId, name: $name)
+    createChannel(teamId: $teamId, name: $name) {
+      ok
+      channel {
+        id
+        name
+      }
+    }
   }
 `;
 
 export default compose(
-  graphql(createTeamMutation),
+  graphql(createChannelMutation),
   withFormik({
     mapPropsToValues: () => ({ name: '' }),
     handleSubmit: async (
       values,
-      { props: { teamId, mutate }, setSubmitting },
+      { props: { teamId, mutate, onClose }, setSubmitting },
     ) => {
-      const response = await mutate({
+      await mutate({
         variables: { teamId, name: values.name },
+        update: (store, { data: { createChannel } }) => {
+          const { ok, channel } = createChannel;
+          if (!ok) return;
+          const data = store.readQuery({ query: allTeamsQuery });
+          const { allTeams } = data;
+          const teamIdx = findIndex(allTeams, team => team.id === teamId);
+          allTeams[teamIdx].channels.push(channel);
+          store.writeQuery({
+            query: allTeamsQuery,
+            data,
+          });
+        },
       });
-      console.log(response);
+      onClose();
       setSubmitting(false);
     },
   }),
