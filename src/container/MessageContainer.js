a071235b02cd8fbb7ba70444027e398eb9eb1e09
ivanberry <tabvim.dev@gmail.com>
2018-06-25 21:50:08 +0800

    .feat Implement  Message container

--- src/container/MessageContainer.js
+++ src/container/MessageContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Comment } from 'semantic-ui-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
@@ -6,7 +7,26 @@ import PropTypes from 'prop-types';
 import Messages from '../components/Messages';
 
 const MessageContainer = ({ data: { loading, messages } }) =>
-  loading ? null : <Messages>{JSON.stringify(messages)}</Messages>;
+  loading ? null : (
+    <Messages>
+      <Comment.Group>
+        {messages.map((m) => (
+          <Comment key={`commect-${m.id}`}>
+            <Comment.Content>
+              <Comment.Author as="a">{m.user.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>{m.created_at}</div>
+              </Comment.Metadata>
+              <Comment.Text>{m.text}</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
+    </Messages>
+  );
 
 const messagesQuery = gql`
   query($channelId: Int!) {
@@ -16,7 +36,7 @@ const messagesQuery = gql`
       user {
         username
       }
-      createdAt
+      created_at
     }
   }
 `;
@@ -24,7 +44,7 @@ const messagesQuery = gql`
 MessageContainer.propTypes = {
   data: PropTypes.shape({
     loading: PropTypes.bool.isRequired,
-    messages: PropTypes.array.isRequired,
+    messages: PropTypes.array,
   }).isRequired,
 };
 
