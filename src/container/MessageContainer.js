2394a5fb68cdaf4bbf01cb74acf5cd99e4b1dd26
ivanberry <tabvim.dev@gmail.com>
2018-06-29 08:38:19 +0800

    .feat unsubscribe the subscription once enter into the same channel

--- src/container/MessageContainer.js
+++ src/container/MessageContainer.js
@@ -22,10 +22,29 @@ const newChannelMessageSubscription = gql`
 
 class MessageContainer extends React.Component {
   componentWillMount() {
+    this.unsubscribe = this.subscribe(this.props.channelId);
+  }
+
+  componentWillReceiveProps({ channelId }) {
+    if (this.props.channelId !== channelId) {
+      if (this.unsubscribe) {
+        this.unsubscribe();
+      }
+      this.unsubscribe = this.subscribe(channelId);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
+
+  subscribe = (channelId) =>
     this.props.data.subscribeToMore({
       document: newChannelMessageSubscription,
       variables: {
-        channelId: this.props.channelId,
+        channelId,
       },
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData) {
@@ -38,7 +57,6 @@ class MessageContainer extends React.Component {
         };
       },
     });
-  }
 
   render() {
     const {
